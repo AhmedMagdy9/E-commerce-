99c5ab4ec80a4002711c92b18f91fe2ca2402b62
AhmedMagdy9 <megoo9786@gmail.com>
2025-03-20 15:40:35 +0200

    إصلاح مشكلة في الـ build

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,9 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+
+  {path:"productdetils/:id" ,  renderMode: RenderMode.Server},
+  {path:"checkOut/:cartID" ,  renderMode: RenderMode.Server},
   {
     path: '**',
     renderMode: RenderMode.Prerender
